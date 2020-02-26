38466e951784604730ef1246b0276b786aa8066f
minami-choi <cmina2846@gmail.com>
2020-02-26 18:07:16 +0900

    백준-부분 수열의 합 (백트래킹으로 해결)

--- dp_1182.js
+++ dp_1182.js
@@ -23,11 +23,47 @@ function solution(params) {
     })
   )
 
-  console.log(answer)
+  console.log(dp, answer)
 }
 
-solution(`5 0
--7 -3 -2 5 8`)
+// solution(`5 0
+// -7 -3 -2 5 8`)
 
 // 부분수열의 합...
 // 연속하지 않아도 됨...
+
+function solution2(params) {
+  const input = params.split("\n")
+  const [n, s] = input
+    .shift()
+    .split(" ")
+    .map(a => +a)
+
+  const array = input[0].split(" ").map(a => +a)
+  // console.log(n, s, array)
+
+  const subArray = []
+  let count = 0
+
+  combi(0)
+  console.log(count)
+
+  // 부분 수열 1~n
+  // 모든 조합의 합이 s인것을 찾으면 됨
+  function combi(start) {
+    const sum = subArray.reduce((a, b) => a + b, 0)
+    if (subArray.length > 0 && sum == s) {
+      // console.log(subArray.join(" "))
+      count++
+    }
+
+    for (let i = start; i < n; i++) {
+      subArray.push(array[i])
+      combi(i + 1)
+      subArray.pop()
+    }
+  }
+}
+
+solution2(`5 0
+-7 -3 -2 5 8`)
