139fd26acee4a14310e10eb4eb0c920b17969080
minami-choi <cmina2846@gmail.com>
2020-03-12 12:12:33 +0900

    백준 괄호

--- stack_9012.js
+++ stack_9012.js
@@ -30,3 +30,31 @@ solution(`6
 ((()()(()))(((())))()
 ()()()()(()()())()
 (()((())()(`)
+
+function solution(params) {
+  const input = params.split("\n")
+  const n = parseInt(input.shift())
+  const pairs = input
+  console.log(n, pairs)
+  for (let i = 0; i < n; i++) {
+    const element = pairs[i].split("")
+    const stack = []
+
+    for (let j = 0; j < element.length; j++) {
+      const item = element[j]
+      if (item == ")") {
+        if (stack[stack.length - 1] == "(") {
+          stack.pop()
+        } else {
+          stack.push(item)
+        }
+      } else {
+        stack.push(item)
+      }
+      // console.log(stack)
+    }
+
+    // console.log(stack)
+    console.log(stack.length > 0 ? "NO" : "YES")
+  }
+}
